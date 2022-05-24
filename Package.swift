b71b3761fb62612340727dc3ac655cd069e9941f
Steven Sherry <steven.sherry@ionic.io>
2022-05-24 09:42:40 -0500

    chore: Update version ranges for Package.swift dependencies to match podspec

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm", .exactItem("3.5.1")),
-        .package(url: "https://github.com/ionic-team/ionic-live-updates-releases", .exactItem("0.1.0")),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm", .upToNextMajor(from: "3.5.1")),
+        .package(url: "https://github.com/ionic-team/ionic-live-updates-releases", .upToNextMinor(from: "0.1.0")),
     ],
     targets: [
         .target(
