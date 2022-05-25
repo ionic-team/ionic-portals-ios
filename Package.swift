37cc062c01f2061edac67b66e735f4913ac812ee
Steven Sherry <steven.sherry@ionic.io>
2022-05-25 09:55:40 -0500

    feat: Support iOS 13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "IonicPortals",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "IonicPortals",
