32b85bc283f392ed35e8221bb07c425bbcc63d68
Steven Sherry <steven.sherry@ionic.io>
2022-10-07 13:55:55 -0500

    chore: Remove tabbed whitespace

--- Sources/IonicPortals/PortalsRegistrationManager.swift
+++ Sources/IonicPortals/PortalsRegistrationManager.swift
@@ -38,10 +38,10 @@ public class PortalsRegistrationManager: NSObject {
         var base64 = base64Url
             .replacingOccurrences(of: "-", with: "+")
             .replacingOccurrences(of: "_", with: "/")
-        
+
         let padding = 4 - base64.count % 4
         guard padding > 0 else { return base64 }
-        
+
         base64 += String(repeating: "=", count: padding)
         return base64
     }
