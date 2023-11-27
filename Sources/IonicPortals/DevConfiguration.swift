f4bada0d3397b879df976242112db3dc5299ce87
Steven Sherry <steven.r.sherry@gmail.com>
2023-11-27 12:47:00 -0600

    chore: The capacitor configuration is now a base64 encoded string that is set in the environment that is decoded and then written to disk.

--- Sources/IonicPortals/DevConfiguration.swift
+++ Sources/IonicPortals/DevConfiguration.swift
@@ -1,17 +1,40 @@
 import Foundation
 
-struct EnvironmentValue {
+struct ServerUrl {
     var variableName: String
     var url: URL? {
         ProcessInfo.processInfo.environment[variableName].flatMap(URL.init)
     }
 }
 
-struct DevConfiguration {
-    var server: EnvironmentValue
-    var capacitorConfig: EnvironmentValue
+struct ConfigUrl {
+    var variableName: String
 
-    init(server: EnvironmentValue, capacitorConfig: EnvironmentValue) {
+    var url: URL? {
+        guard let value = ProcessInfo.processInfo.environment[variableName],
+              let decodedData = Data(base64Encoded: value, options: [.ignoreUnknownCharacters]),
+              let decodedString = String(data: decodedData, encoding: .utf8)
+        else { return nil }
+
+        let targetTempFile = FileManager.default.temporaryDirectory
+            .appendingPathComponent(UUID().uuidString)
+            .appendingPathExtension("json")
+
+        do {
+            try decodedString.write(to: targetTempFile, atomically: true, encoding: .utf8)
+        } catch {
+            return nil
+        }
+
+        return targetTempFile
+    }
+}
+
+struct DevConfiguration {
+    var server: ServerUrl
+    var capacitorConfig: ConfigUrl
+
+    init(server: ServerUrl, capacitorConfig: ConfigUrl) {
         self.server = server
         self.capacitorConfig = capacitorConfig
     }
